8cc87a0f92875be94c2ba731482b0400bb65d4e3
Daryna Shevchuk <d.shevchuk.pn@ucu.edu.ua>
2026-03-12 21:44:46 +0200

    added the twelve task

---
+++ unique_paths.py
@@ -0,0 +1,16 @@
+"""Exercise from Leetcode"""
+
+def unique_paths(m: int, n: int) -> int:
+    """."""
+
+    dp = [[1]*n for _ in range(m)]
+    for i in range(1, m):
+        for j in range(1, n):
+            dp[i][j] = dp[i-1][j] + dp[i][j-1]
+
+    return dp[m-1][n-1]
+
+if __name__ == "__main__":
+    print(unique_paths(3, 7))  # 28
+    print(unique_paths(3, 2))  # 3
+    print(unique_paths(1, 1))  # 1
